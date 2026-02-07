5198d3a30a167741300f3a28cf3c706fcf3d42fc
Shushma21 <shushma972001@gmail.com>
2026-02-07 12:46:03 +0530

    Added users app with UserProfile model

--- user_management/models.py
+++ user_management/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+	email = models.EmailField(unique = True)
+	fullname = models.CharField(max_length=100)
+	is_active = models.BooleanField(default=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.email
